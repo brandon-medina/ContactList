22602dc27d2421363346022f79c1273e3a624ee2
brandon-medina <brandon.k.medina@outlook.com>
2024-04-13 16:41:33 -0500

    save point

---
+++ components/SelectedContact.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function selectedContactId({ contactId }) {
+
+    const [selectedContactId, setSelectedContactId] = useState(null);
+
+    return (
+      
+    );
+  }
